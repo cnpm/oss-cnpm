8cfa47be3950afbf06ad0069bab6423bfc360855
fengmk2 <m@fengmk2.com>
2015-09-29 19:38:42 +0800

    feat: support oss cluster client

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 /**!
- * oss-cnpm - index.js
- *
- * Copyright(c) cnpmjs.org and other contributors.
+ * Copyright(c) cnpm and other contributors.
  * MIT Licensed
  *
  * Authors:
  *  dead_horse <dead_horse@qq.com> (http://deadhorse.me)
- *  fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ *  fengmk2 <fengmk2@gmail.com> (http://fengmk2.com)
  */
 
 'use strict';
@@ -15,21 +13,30 @@
  * Module dependencies.
  */
 
-var oss = require('ali-oss');
-var path = require('path');
+const oss = require('ali-oss');
+const path = require('path');
+const is = require('is-type-of');
 
 exports.create = function (options) {
   return new OssWrapper(options);
 };
 
 function OssWrapper(options) {
-  this.client = oss(options);
   // If you want to use oss public mode, please set `options.mode = 'public'`
   this._mode = options.mode === 'public' ? 'public' : 'private';
   this._publicRoot = options.publicRoot; // 'cnpm.oss.aliyuncs.com'
+
+  if (options.cluster) {
+    options.schedule = options.schedule || 'masterSlave';
+    this.client = new oss.ClusterClient(options);
+  } else {
+    this.client = oss(options);
+  }
+
   if (this._mode === 'public') {
     if (!this._publicRoot) {
-      this._publicRoot = 'http://' + options.bucket + '.oss.aliyuncs.com';
+      const endpoint = options.endpoint || 'oss.aliyuncs.com';
+      this._publicRoot = 'http://' + options.bucket + '.' + endpoint;
     } else {
       if (this._publicRoot.indexOf('://') < 0) {
         this._publicRoot = 'http://' + this._publicRoot;
@@ -39,13 +46,13 @@ function OssWrapper(options) {
   }
 }
 
-var proto = OssWrapper.prototype;
+const proto = OssWrapper.prototype;
 
 proto.upload = function* (filePath, options) {
-  var key = trimKey(options.key);
-  var res = yield this.client.put(key, filePath);
+  const key = trimKey(options.key);
+  const result = yield this.client.put(key, filePath);
   if (this._mode === 'public') {
-    return { url: this._publicRoot + '/' + key };
+    return { url: result.url };
   }
   return { key: key };
 };
@@ -53,19 +60,26 @@ proto.upload = function* (filePath, options) {
 proto.uploadBuffer = proto.upload;
 
 proto.download = function* (key, filepath, options) {
-  yield* this.client.get(trimKey(key), filepath, options);
+  yield this.client.get(trimKey(key), filepath, options);
 };
 
 proto.createDownloadStream = function* (key, options) {
   return (yield this.client.getStream(trimKey(key), options)).stream;
 };
 
-proto.url = function (key) {
-  return this.client.signatureUrl(trimKey(key));
+proto.url = function* (key) {
+  const name = trimKey(key);
+  // cluster client
+  if (is.generatorFunction(this.client.signatureUrl)) {
+    return yield this.client.signatureUrl(name);
+  }
+
+  // normal client
+  return this.client.signatureUrl(name);
 };
 
 proto.remove = function* (key) {
-  yield* this.client.delete(trimKey(key));
+  yield this.client.delete(trimKey(key));
 };
 
 function trimKey(key) {
