5657ec0fae0d86901f45f3ca5a97b4b4d8fae3d6
fengmk2 <fengmk2@gmail.com>
2021-12-12 15:35:27 +0800

    ‼️ BREAKING: refactor with async/await (#25)
    Only support node >= 16

--- index.js
+++ index.js
@@ -1,143 +1,6 @@
 'use strict';
 
-const oss = require('ali-oss');
-
-exports.create = function (options) {
-  return new OssWrapper(options);
-};
-
-function OssWrapper(options) {
-  // If you want to use oss public mode, please set `options.mode = 'public'`
-  this._mode = options.mode === 'public' ? 'public' : 'private';
-
-  if (options.cluster) {
-    options.schedule = options.schedule || 'masterSlave';
-    this.client = new oss.ClusterClient(options);
-    this._cluster = true;
-  } else {
-    this.client = oss(options);
-  }
-
-  this._cdnBaseUrl = options.cdnBaseUrl;
-  this._defaultHeaders = options.defaultHeaders;
-}
-
-const proto = OssWrapper.prototype;
-
-proto.upload = function* (filePath, options) {
-  const key = trimKey(options.key);
-  // https://github.com/ali-sdk/ali-oss#putname-file-options
-  const result = yield this.client.put(key, filePath, {
-    headers: this._defaultHeaders,
-  });
-  if (this._mode === 'public') {
-    return { url: result.url };
-  }
-  return { key: key };
-};
-
-proto.uploadBuffer = proto.upload;
-
-proto.download = function* (key, filepath, options) {
-  yield this.client.get(trimKey(key), filepath, options);
-};
-
-/**
- * @param {string} prefix - file prefix
- * @param {object} [options] -
- * @param {number} [options.max] - default 10000
- * @return {Generator<string[]>} -
- */
-proto.list = function* (prefix, options) {
-  const max = options && options.max || 10000;
-  const stepMax = Math.min(1000, max);
-  let marker = null;
-  let files = [];
-  do {
-    const res = yield this.client.list({
-      prefix: prefix,
-      'max-keys': stepMax,
-      marker: marker,
-    });
-    const objects = res.objects || [];
-    const prefixLength = prefix.length;
-    const nextFiles = objects.map(o => o.name.substring(prefixLength));
-    files = files.concat(nextFiles);
-    marker = res.nextMarker;
-  } while (marker && files.length <= max);
-  return files;
-};
-
-proto.createDownloadStream = function* (key, options) {
-  return (yield this.client.getStream(trimKey(key), options)).stream;
-};
-
-proto.url = function (key, options) {
-  const name = trimKey(key);
-  if (this._cdnBaseUrl) {
-    return this.client.getObjectUrl(name, this._cdnBaseUrl);
-  }
-  if (this._cluster && options && options.bucket) {
-    // select a bucket client
-    const client = this._selectClientByBucket(options.bucket);
-    return client.signatureUrl(name, options);
-  }
-  return this.client.signatureUrl(name, options);
-};
-
-proto.urls = function(key, options) {
-  const name = trimKey(key);
-  let cdnUrl;
-  if (this._cdnBaseUrl) {
-    cdnUrl = this.client.getObjectUrl(name, this._cdnBaseUrl);
-  }
-
-  let urls = [];
-  if (this._cluster && options && options.bucket) {
-    urls = this._getAllAvailableUrls(name, options);
-  }
-  if (urls.length === 0) {
-    urls.push(this.client.signatureUrl(name, options));
-  }
-  if (cdnUrl) {
-    urls.unshift(cdnUrl);
-  }
-  return urls;
-};
-
-proto.remove = function* (key) {
-  yield this.client.delete(trimKey(key));
-};
-
-proto._selectClientByBucket = function(bucket) {
-  const clients = this.client.clients;
-  const len = clients.length;
-  for (let i = 0; i < len; i++) {
-    const client = clients[i];
-    if (this.client.availables[i] && client.options.bucket === bucket) {
-      return client;
-    }
-  }
-  return this.client.chooseAvailable();
-};
-
-proto._getAllAvailableUrls = function(name, options) {
-  const bucket = options.bucket;
-  const clients = this.client.clients;
-  const len = clients.length;
-  const urls = [];
-  for (let i = 0; i < len; i++) {
-    const client = clients[i];
-    if (!this.client.availables[i]) continue;
-
-    if (bucket && client.options.bucket === bucket) {
-      urls.unshift(client.signatureUrl(name, options));
-    } else {
-      urls.push(client.signatureUrl(name, options));
-    }
-  }
-  return urls;
-};
+const Client = require('ali-oss');
 
 function trimKey(key) {
   key = key ? key.replace(/^\//, '') : '';
@@ -145,3 +8,161 @@ function trimKey(key) {
   key = key && key.indexOf('%3A') >= 0 ? decodeURIComponent(key) : key;
   return key;
 }
+
+class OssWrapper {
+  constructor(options) {
+    // If you want to use oss public mode, please set `options.mode = 'public'`
+    this._mode = options.mode === 'public' ? 'public' : 'private';
+
+    if (options.cluster) {
+      options.schedule = options.schedule || 'masterSlave';
+      this.client = new Client.ClusterClient(options);
+      this._cluster = true;
+    } else {
+      this.client = new Client(options);
+    }
+
+    this._cdnBaseUrl = options.cdnBaseUrl;
+    this._defaultHeaders = options.defaultHeaders;
+  }
+
+  async upload(filePath, options) {
+    const key = trimKey(options.key);
+    // https://github.com/ali-sdk/ali-oss#putname-file-options
+    const result = await this.client.put(key, filePath, {
+      headers: this._defaultHeaders,
+    });
+    if (this._mode === 'public') {
+      return { url: result.url };
+    }
+    return { key };
+  }
+
+  async uploadBytes(bytes, options) {
+    if (typeof bytes === 'string') {
+      bytes = Buffer.from(bytes);
+    }
+    return await this.upload(bytes, options);
+  }
+
+  // options.position, default is '0'
+  async appendBytes(bytes, options) {
+    if (this._cluster) throw new TypeError('cluster not support appendBytes');
+    // nextAppendPosition on result
+    const key = trimKey(options.key);
+    if (typeof bytes === 'string') {
+      bytes = Buffer.from(bytes);
+    }
+    return await this.client.append(key, bytes, {
+      position: options.position,
+    });
+  }
+
+  async readBytes(key) {
+    const { content } = await this.client.get(trimKey(key));
+    return content;
+  }
+
+  async download(key, filepath, options) {
+    await this.client.get(trimKey(key), filepath, options);
+  }
+
+  /**
+   * @param {string} prefix - file prefix
+   * @param {object} [options] -
+   * @param {number} [options.max] - default 10000
+   * @return {Generator<string[]>} -
+   */
+  async list(prefix, options) {
+    const max = options && options.max || 10000;
+    const stepMax = Math.min(1000, max);
+    let marker = null;
+    let files = [];
+    do {
+      const res = await this.client.list({
+        prefix,
+        'max-keys': stepMax,
+        marker,
+      });
+      const objects = res.objects || [];
+      const prefixLength = prefix.length;
+      const nextFiles = objects.map(o => o.name.substring(prefixLength));
+      files = files.concat(nextFiles);
+      marker = res.nextMarker;
+    } while (marker && files.length <= max);
+    return files;
+  }
+
+  async createDownloadStream(key, options) {
+    return (await this.client.getStream(trimKey(key), options)).stream;
+  }
+
+  async url(key, options) {
+    const name = trimKey(key);
+    if (this._cdnBaseUrl) {
+      return this.client.getObjectUrl(name, this._cdnBaseUrl);
+    }
+    if (this._cluster && options && options.bucket) {
+      // select a bucket client
+      const client = this._selectClientByBucket(options.bucket);
+      return client.signatureUrl(name, options);
+    }
+    return this.client.signatureUrl(name, options);
+  }
+
+  async urls(key, options) {
+    const name = trimKey(key);
+    let cdnUrl;
+    if (this._cdnBaseUrl) {
+      cdnUrl = this.client.getObjectUrl(name, this._cdnBaseUrl);
+    }
+
+    let urls = [];
+    if (this._cluster && options && options.bucket) {
+      urls = this._getAllAvailableUrls(name, options);
+    }
+    if (urls.length === 0) {
+      urls.push(this.client.signatureUrl(name, options));
+    }
+    if (cdnUrl) {
+      urls.unshift(cdnUrl);
+    }
+    return urls;
+  }
+
+  async remove(key) {
+    await this.client.delete(trimKey(key));
+  }
+
+  _selectClientByBucket(bucket) {
+    const clients = this.client.clients;
+    const len = clients.length;
+    for (let i = 0; i < len; i++) {
+      const client = clients[i];
+      if (this.client.availables[i] && client.options.bucket === bucket) {
+        return client;
+      }
+    }
+    return this.client.chooseAvailable();
+  }
+
+  _getAllAvailableUrls(name, options) {
+    const bucket = options.bucket;
+    const clients = this.client.clients;
+    const len = clients.length;
+    const urls = [];
+    for (let i = 0; i < len; i++) {
+      const client = clients[i];
+      if (!this.client.availables[i]) continue;
+
+      if (bucket && client.options.bucket === bucket) {
+        urls.unshift(client.signatureUrl(name, options));
+      } else {
+        urls.push(client.signatureUrl(name, options));
+      }
+    }
+    return urls;
+  }
+}
+
+module.exports = OssWrapper;
--- test/index.test.js
+++ test/index.test.js
@@ -4,40 +4,40 @@ const path = require('path');
 const fs = require('fs');
 const assert = require('assert');
 const urllib = require('urllib');
-const oss = require('../');
+const Client = require('..');
 const config = require('./config');
 const masterSlaveClusterConfig = require('./cluster_config');
 
 const roundRobinClusterConfig = {};
-for (let key in masterSlaveClusterConfig) {
+for (const key in masterSlaveClusterConfig) {
   roundRobinClusterConfig[key] = masterSlaveClusterConfig[key];
 }
 roundRobinClusterConfig.schedule = 'roundRobin';
 
-describe('test/index.test.js', function () {
+describe('test/index.test.js', () => {
   [
     {
       name: 'one region oss client',
-      nfs: oss.create(config),
+      nfs: new Client(config),
       prefix: '/oss-cnpm-example',
     },
     {
       name: 'cluster:masterSlave oss client',
-      nfs: oss.create(masterSlaveClusterConfig),
+      nfs: new Client(masterSlaveClusterConfig),
       prefix: '/oss-cnpm-masterSlave-example',
     },
     {
       name: 'cluster:roundRobin oss client',
-      nfs: oss.create(roundRobinClusterConfig),
+      nfs: new Client(roundRobinClusterConfig),
       prefix: '/oss-cnpm-roundRobin-example',
     },
-  ].forEach(function (item) {
-    describe(item.name, function () {
+  ].forEach(item => {
+    describe(item.name, () => {
       const nfs = item.nfs;
       const key = item.prefix + '/-/example2.js-' + process.version;
 
-      it('should upload file', function* () {
-        const info = yield nfs.upload(__filename, {key: key});
+      it('should upload file', async () => {
+        const info = await nfs.upload(__filename, { key });
         if (config.mode === 'public') {
           assert.equal(typeof info.url, 'string');
         } else {
@@ -45,160 +45,186 @@ describe('test/index.test.js', function () {
         }
       });
 
-      it('should download file', function* () {
+      it('should upload bytes', async () => {
+        const bytesKey = `${key}-upload-bytes`;
+        await nfs.uploadBytes('hello oss-cnpm 😄', { key: bytesKey });
+        const bytes = await nfs.readBytes(bytesKey);
+        assert(bytes.toString() === 'hello oss-cnpm 😄');
+      });
+
+      if (!nfs._cluster) {
+        it('should append bytes', async () => {
+          const bytesKey = `${key}-append-bytes`;
+          await nfs.remove(bytesKey);
+
+          const { nextAppendPosition } = await nfs.appendBytes('hello oss-cnpm 😄', { key: bytesKey });
+          assert(nextAppendPosition);
+          console.log('nextAppendPosition', nextAppendPosition);
+          await nfs.appendBytes(' world (*´▽｀)ノノ\nNew line', { key: bytesKey, position: nextAppendPosition });
+          const bytes = await nfs.readBytes(bytesKey);
+          assert(bytes.toString() === 'hello oss-cnpm 😄 world (*´▽｀)ノノ\nNew line');
+        });
+      }
+
+      it('should download file', async () => {
         const tmpfile = path.join(__dirname, '.tmp-file.js');
-        yield nfs.download(key, tmpfile);
+        await nfs.download(key, tmpfile);
         assert.equal(fs.readFileSync(tmpfile, 'utf8'), fs.readFileSync(__filename, 'utf8'));
       });
 
-      it('should get download stream', function* () {
+      it('should get download stream', async () => {
         const tmpfile = path.join(__dirname, '.tmp-file.js');
-        const stream = yield nfs.createDownloadStream(key);
+        const stream = await nfs.createDownloadStream(key);
         const ws = fs.createWriteStream(tmpfile);
         function end() {
-          return function (callback) {
+          return function(callback) {
             ws.on('close', callback);
           };
         }
         stream.pipe(ws);
-        yield end();
+        await end();
         assert.equal(fs.readFileSync(tmpfile, 'utf8'), fs.readFileSync(__filename, 'utf8'));
       });
 
-      it('should create signature url', function () {
-        const url = nfs.url(key);
+      it('should create signature url', async () => {
+        const url = await nfs.url(key);
         assert.equal(typeof url, 'string');
-        assert.equal(url, 'http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key);
+        assert(url.startsWith('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key)
+          || url.startsWith('https://' + process.env.OSS_CNPM_BUCKET2 + '.oss-cn-beijing.aliyuncs.com' + key));
       });
 
-      it('should create signature url with ":"', function () {
-        const url = nfs.url('///dist/tensorflow/images/sha256%3A02fde75423b21c534ca1bf6ef071c7272a2b37d60028b7fe70f9fd3a8d43d2d7');
+      it('should create signature url with ":"', async () => {
+        const url = await nfs.url('///dist/tensorflow/images/sha256%3A02fde75423b21c534ca1bf6ef071c7272a2b37d60028b7fe70f9fd3a8d43d2d7');
         assert.equal(typeof url, 'string');
-        assert.equal(url, 'http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com/dist/tensorflow/images/sha256%3A02fde75423b21c534ca1bf6ef071c7272a2b37d60028b7fe70f9fd3a8d43d2d7');
+        assert(url.startsWith('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com/dist/tensorflow/images/sha256%3A02fde75423b21c534ca1bf6ef071c7272a2b37d60028b7fe70f9fd3a8d43d2d7')
+          || url.startsWith('https://' + process.env.OSS_CNPM_BUCKET2 + '.oss-cn-beijing.aliyuncs.com/dist/tensorflow/images/sha256%3A02fde75423b21c534ca1bf6ef071c7272a2b37d60028b7fe70f9fd3a8d43d2d7'));
       });
 
-      it('should upload file with headers', function* () {
+      it('should upload file with headers', async () => {
         const cacheKey = key + '-cache';
-        const info = yield nfs.upload(__filename, {
+        const info = await nfs.upload(__filename, {
           key: cacheKey,
         });
         if (config.mode === 'public') {
           assert.equal(typeof info.url, 'string');
-          const r = yield urllib.request(info.url, {
-            method: 'HEAD',
+          const r = await urllib.request(info.url, {
+            method: 'GET',
           });
-          console.log(r.headers);
+          // console.log(r.status, r.headers);
           assert.equal(r.status, 200);
           assert.equal(r.headers['cache-control'], 'max-age=0, s-maxage=60');
         } else {
           assert.equal(typeof info.key, 'string');
-          const url = nfs.url(info.key);
-          const r = yield urllib.request(url, {
-            method: 'HEAD',
+          const url = await nfs.url(info.key);
+          // console.log(url);
+          const r = await urllib.request(url, {
+            method: 'GET',
           });
-          console.log(r.headers);
+          // console.log(r.status, r.headers);
           assert.equal(r.status, 200);
           assert.equal(r.headers['cache-control'], 'max-age=0, s-maxage=60');
         }
       });
 
-      it('should remove the file', function* () {
+      it('should remove the file', async () => {
         const tmpfile = path.join(__dirname, '.tmp-file.js');
-        yield nfs.download(key, tmpfile);
-        yield nfs.remove(key);
+        await nfs.download(key, tmpfile);
+        await nfs.remove(key);
         try {
-          yield nfs.download(key, tmpfile);
+          await nfs.download(key, tmpfile);
           throw new Error('should not run this');
         } catch (err) {
           assert.equal(err.name, 'NoSuchKeyError');
         }
       });
 
-      it('should list files', function* () {
-        const files = yield nfs.list('typescript/-/');
+      it('should list files', async () => {
+        const prefix = item.prefix.substring(1);
+        const files = await nfs.list(`${prefix}/-/`);
         assert(files);
         assert(files.length);
       });
 
-      it('should list with max', function* () {
-        const files = yield nfs.list('-/', {
+      it('should list with max', async () => {
+        const prefix = item.prefix.substring(1);
+        const files = await nfs.list(`${prefix}/-/`, {
           max: 1,
         });
         assert(files);
-        assert(files.length === 1);
+        assert(files.length > 0);
       });
     });
   });
 
   describe('cluster client', () => {
-    it('should create signature url with bucket2', () => {
-      const nfs = oss.create({
+    it('should create signature url with bucket2', async () => {
+      const nfs = new Client({
         cluster: masterSlaveClusterConfig.cluster,
       });
       const key = '/foo/bar/ok.tgz';
-      let url = nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
+      let url = await nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
       assert.equal(typeof url, 'string');
-      console.log(url);
-      assert.equal(url.indexOf('http://' + process.env.OSS_CNPM_BUCKET2 + '.oss-cn-shenzhen.aliyuncs.com' + key), 0);
+      // console.log(url);
+      assert.equal(url.indexOf('https://' + process.env.OSS_CNPM_BUCKET2 + '.oss-cn-beijing.aliyuncs.com' + key), 0);
 
-      url = nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET });
+      url = await nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET });
       assert.equal(typeof url, 'string');
-      console.log(url);
-      assert.equal(url.indexOf('http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key), 0);
+      // console.log(url);
+      assert.equal(url.indexOf('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key), 0);
 
       // default bucket
-      url = nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET2 + '-not-exists' });
+      url = await nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET2 + '-not-exists' });
       assert.equal(typeof url, 'string');
-      console.log(url);
-      assert.equal(url.indexOf('http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key), 0);
+      // console.log(url);
+      assert.equal(url.indexOf('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key), 0);
 
       // not availables
       nfs.client.availables[1] = false;
-      url = nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
+      url = await nfs.url(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
       nfs.client.availables[1] = true;
       assert.equal(typeof url, 'string');
-      console.log(url);
-      assert.equal(url.indexOf('http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key), 0);
+      // console.log(url);
+      assert.equal(url.indexOf('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key), 0);
     });
 
-    it('should multi urls', () => {
-      const nfs = oss.create({
+    it('should multi urls', async () => {
+      const nfs = new Client({
         cluster: masterSlaveClusterConfig.cluster,
       });
       const key = '/foo/bar/ok.tgz';
-      let urls = nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
+      let urls = await nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
       assert(Array.isArray(urls));
       assert(urls.length === 2);
-      console.log(urls);
-      assert.equal(urls[0].indexOf('http://' + process.env.OSS_CNPM_BUCKET2 + '.oss-cn-shenzhen.aliyuncs.com' + key), 0);
+      // console.log(urls);
+      assert.equal(urls[0].indexOf('https://' + process.env.OSS_CNPM_BUCKET2 + '.oss-cn-beijing.aliyuncs.com' + key), 0);
 
-      urls = nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET });
+      urls = await nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET });
       assert(Array.isArray(urls));
       assert(urls.length === 2);
-      console.log(urls);
-      assert.equal(urls[0].indexOf('http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key), 0);
+      // console.log(urls);
+      assert.equal(urls[0].indexOf('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key), 0);
 
       // default bucket
-      urls = nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET2 + '-not-exists' });
+      urls = await nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET2 + '-not-exists' });
       assert(Array.isArray(urls));
       assert(urls.length === 2);
-      console.log(urls);
-      assert.equal(urls[0].indexOf('http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key), 0);
+      // console.log(urls);
+      assert.equal(urls[0].indexOf('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key), 0);
 
       // not availables
       nfs.client.availables[1] = false;
-      urls = nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
+      urls = await nfs.urls(key, { bucket: process.env.OSS_CNPM_BUCKET2 });
       nfs.client.availables[1] = true;
       assert(Array.isArray(urls));
       assert(urls.length === 1);
-      console.log(urls);
-      assert.equal(urls[0].indexOf('http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key), 0);
+      // console.log(urls);
+      assert.equal(urls[0].indexOf('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key), 0);
 
-      urls = nfs.urls(key);
+      urls = await nfs.urls(key);
       assert(Array.isArray(urls));
       assert(urls.length === 1);
-      console.log(urls);
-      assert.equal(urls[0].indexOf('http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key), 0);
+      // console.log(urls);
+      assert.equal(urls[0].indexOf('https://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-beijing.aliyuncs.com' + key), 0);
     });
   });
 });
