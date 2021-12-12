aa1eeaee2afeda8ea03d20554379ca71cfe286af
fengmk2 <fengmk2@gmail.com>
2021-12-12 13:37:26 +0800

    ‼️ BREAKING: refactor with async/await
    Only support node >= 16

--- test/index.test.js
+++ test/index.test.js
@@ -4,40 +4,40 @@ const path = require('path');
 const fs = require('fs');
 const assert = require('assert');
 const urllib = require('urllib');
-const oss = require('../');
+const Client = require('..');
 const config = require('./config');
 const masterSlaveClusterConfig = require('./cluster_config');
 
 const roundRobinClusterConfig = {};
-for (let key in masterSlaveClusterConfig) {
+for (const key in masterSlaveClusterConfig) {
   roundRobinClusterConfig[key] = masterSlaveClusterConfig[key];
 }
 roundRobinClusterConfig.schedule = 'roundRobin';
 
-describe('test/index.test.js', function () {
+describe('test/index.test.js', () => {
   [
     {
       name: 'one region oss client',
-      nfs: oss.create(config),
+      nfs: new Client(config),
       prefix: '/oss-cnpm-example',
     },
     {
       name: 'cluster:masterSlave oss client',
-      nfs: oss.create(masterSlaveClusterConfig),
+      nfs: new Client(masterSlaveClusterConfig),
       prefix: '/oss-cnpm-masterSlave-example',
     },
     {
       name: 'cluster:roundRobin oss client',
-      nfs: oss.create(roundRobinClusterConfig),
+      nfs: new Client(roundRobinClusterConfig),
       prefix: '/oss-cnpm-roundRobin-example',
     },
-  ].forEach(function (item) {
-    describe(item.name, function () {
+  ].forEach(item => {
+    describe(item.name, () => {
       const nfs = item.nfs;
       const key = item.prefix + '/-/example2.js-' + process.version;
 
-      it('should upload file', function* () {
-        const info = yield nfs.upload(__filename, {key: key});
+      it('should upload file', async () => {
+        const info = await nfs.upload(__filename, { key });
         if (config.mode === 'public') {
           assert.equal(typeof info.url, 'string');
         } else {
@@ -45,46 +45,46 @@ describe('test/index.test.js', function () {
         }
       });
 
-      it('should download file', function* () {
+      it('should download file', async () => {
         const tmpfile = path.join(__dirname, '.tmp-file.js');
-        yield nfs.download(key, tmpfile);
+        await nfs.download(key, tmpfile);
         assert.equal(fs.readFileSync(tmpfile, 'utf8'), fs.readFileSync(__filename, 'utf8'));
       });
 
-      it('should get download stream', function* () {
+      it('should get download stream', async () => {
         const tmpfile = path.join(__dirname, '.tmp-file.js');
-        const stream = yield nfs.createDownloadStream(key);
+        const stream = await nfs.createDownloadStream(key);
         const ws = fs.createWriteStream(tmpfile);
         function end() {
-          return function (callback) {
+          return function(callback) {
             ws.on('close', callback);
           };
         }
         stream.pipe(ws);
-        yield end();
+        await end();
         assert.equal(fs.readFileSync(tmpfile, 'utf8'), fs.readFileSync(__filename, 'utf8'));
       });
 
-      it('should create signature url', function () {
+      it('should create signature url', function() {
         const url = nfs.url(key);
         assert.equal(typeof url, 'string');
         assert.equal(url, 'http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com' + key);
       });
 
-      it('should create signature url with ":"', function () {
+      it('should create signature url with ":"', function() {
         const url = nfs.url('///dist/tensorflow/images/sha256%3A02fde75423b21c534ca1bf6ef071c7272a2b37d60028b7fe70f9fd3a8d43d2d7');
         assert.equal(typeof url, 'string');
         assert.equal(url, 'http://' + process.env.OSS_CNPM_BUCKET + '.oss-cn-hangzhou.aliyuncs.com/dist/tensorflow/images/sha256%3A02fde75423b21c534ca1bf6ef071c7272a2b37d60028b7fe70f9fd3a8d43d2d7');
       });
 
-      it('should upload file with headers', function* () {
+      it('should upload file with headers', async () => {
         const cacheKey = key + '-cache';
-        const info = yield nfs.upload(__filename, {
+        const info = await nfs.upload(__filename, {
           key: cacheKey,
         });
         if (config.mode === 'public') {
           assert.equal(typeof info.url, 'string');
-          const r = yield urllib.request(info.url, {
+          const r = await urllib.request(info.url, {
             method: 'HEAD',
           });
           console.log(r.headers);
@@ -93,7 +93,7 @@ describe('test/index.test.js', function () {
         } else {
           assert.equal(typeof info.key, 'string');
           const url = nfs.url(info.key);
-          const r = yield urllib.request(url, {
+          const r = await urllib.request(url, {
             method: 'HEAD',
           });
           console.log(r.headers);
@@ -102,26 +102,26 @@ describe('test/index.test.js', function () {
         }
       });
 
-      it('should remove the file', function* () {
+      it('should remove the file', async () => {
         const tmpfile = path.join(__dirname, '.tmp-file.js');
-        yield nfs.download(key, tmpfile);
-        yield nfs.remove(key);
+        await nfs.download(key, tmpfile);
+        await nfs.remove(key);
         try {
-          yield nfs.download(key, tmpfile);
+          await nfs.download(key, tmpfile);
           throw new Error('should not run this');
         } catch (err) {
           assert.equal(err.name, 'NoSuchKeyError');
         }
       });
 
-      it('should list files', function* () {
-        const files = yield nfs.list('typescript/-/');
+      it('should list files', async () => {
+        const files = await nfs.list('typescript/-/');
         assert(files);
         assert(files.length);
       });
 
-      it('should list with max', function* () {
-        const files = yield nfs.list('-/', {
+      it('should list with max', async () => {
+        const files = await nfs.list('-/', {
           max: 1,
         });
         assert(files);
@@ -132,7 +132,7 @@ describe('test/index.test.js', function () {
 
   describe('cluster client', () => {
     it('should create signature url with bucket2', () => {
-      const nfs = oss.create({
+      const nfs = new Client({
         cluster: masterSlaveClusterConfig.cluster,
       });
       const key = '/foo/bar/ok.tgz';
@@ -162,7 +162,7 @@ describe('test/index.test.js', function () {
     });
 
     it('should multi urls', () => {
-      const nfs = oss.create({
+      const nfs = new Client({
         cluster: masterSlaveClusterConfig.cluster,
       });
       const key = '/foo/bar/ok.tgz';
