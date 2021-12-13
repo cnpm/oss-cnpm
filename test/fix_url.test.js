79a0a6a1bfc080017dd000f72f3b1db8a38b3a6f
fengmk2 <fengmk2@gmail.com>
2021-12-13 22:20:36 +0800

    🐛 FIX: Autofix internal oss endpoint (#27)
    https://github.com/cnpm/cnpmcore/pull/30

---
+++ test/fix_url.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert');
+const Client = require('..');
+const config = require('./config');
+
+describe('test/fix_url.test.js', () => {
+  it('should auto fix internal oss url to public', async () => {
+    const nfs = new Client({
+      ...config,
+      endpoint: 'https://oss-cn-beijing-internal.aliyuncs.com',
+    });
+    const url = await nfs.url('foo/bar.txt');
+    assert(url.startsWith('https://oss-cnpm-unittest1.oss-cn-beijing.aliyuncs.com/foo/bar.txt?OSSAccessKeyId='));
+  });
+});
